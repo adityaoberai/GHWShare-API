4130279b51a7eb3664c8438cb51f20a96453dd13
adityaoberai <adityaoberai1@gmail.com>
2022-04-06 15:06:06 +0530

    Create project on livestream

---
+++ OxfordDictionaryResponse.cs
@@ -0,0 +1,252 @@
+// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+    public class Metadata
+    {
+        [JsonProperty("operation")]
+        public string Operation { get; set; }
+
+        [JsonProperty("provider")]
+        public string Provider { get; set; }
+
+        [JsonProperty("schema")]
+        public string Schema { get; set; }
+    }
+
+    public class Derivative
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+
+    public class GrammaticalFeature
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+
+    public class Note
+    {
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+
+    public class Pronunciation
+    {
+        [JsonProperty("audioFile")]
+        public string AudioFile { get; set; }
+
+        [JsonProperty("dialects")]
+        public List<string> Dialects { get; set; }
+
+        [JsonProperty("phoneticNotation")]
+        public string PhoneticNotation { get; set; }
+
+        [JsonProperty("phoneticSpelling")]
+        public string PhoneticSpelling { get; set; }
+    }
+
+    public class Example
+    {
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+
+    public class SemanticClass
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+
+    public class DomainClass
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+
+    public class Subsens
+    {
+        [JsonProperty("definitions")]
+        public List<string> Definitions { get; set; }
+
+        [JsonProperty("examples")]
+        public List<Example> Examples { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("shortDefinitions")]
+        public List<string> ShortDefinitions { get; set; }
+
+        [JsonProperty("semanticClasses")]
+        public List<SemanticClass> SemanticClasses { get; set; }
+
+        [JsonProperty("domainClasses")]
+        public List<DomainClass> DomainClasses { get; set; }
+
+        [JsonProperty("notes")]
+        public List<Note> Notes { get; set; }
+    }
+
+    public class Synonym
+    {
+        [JsonProperty("language")]
+        public string Language { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+
+    public class ThesaurusLink
+    {
+        [JsonProperty("entry_id")]
+        public string EntryId { get; set; }
+
+        [JsonProperty("sense_id")]
+        public string SenseId { get; set; }
+    }
+
+    public class Sens
+    {
+        [JsonProperty("definitions")]
+        public List<string> Definitions { get; set; }
+
+        [JsonProperty("examples")]
+        public List<Example> Examples { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("shortDefinitions")]
+        public List<string> ShortDefinitions { get; set; }
+
+        [JsonProperty("subsenses")]
+        public List<Subsens> Subsenses { get; set; }
+
+        [JsonProperty("synonyms")]
+        public List<Synonym> Synonyms { get; set; }
+
+        [JsonProperty("thesaurusLinks")]
+        public List<ThesaurusLink> ThesaurusLinks { get; set; }
+
+        [JsonProperty("semanticClasses")]
+        public List<SemanticClass> SemanticClasses { get; set; }
+
+        [JsonProperty("domainClasses")]
+        public List<DomainClass> DomainClasses { get; set; }
+    }
+
+    public class Entry
+    {
+        [JsonProperty("etymologies")]
+        public List<string> Etymologies { get; set; }
+
+        [JsonProperty("grammaticalFeatures")]
+        public List<GrammaticalFeature> GrammaticalFeatures { get; set; }
+
+        [JsonProperty("homographNumber")]
+        public string HomographNumber { get; set; }
+
+        [JsonProperty("notes")]
+        public List<Note> Notes { get; set; }
+
+        [JsonProperty("pronunciations")]
+        public List<Pronunciation> Pronunciations { get; set; }
+
+        [JsonProperty("senses")]
+        public List<Sens> Senses { get; set; }
+    }
+
+    public class LexicalCategory
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+
+    public class Phras
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+
+    public class LexicalEntry
+    {
+        [JsonProperty("derivatives")]
+        public List<Derivative> Derivatives { get; set; }
+
+        [JsonProperty("entries")]
+        public List<Entry> Entries { get; set; }
+
+        [JsonProperty("language")]
+        public string Language { get; set; }
+
+        [JsonProperty("lexicalCategory")]
+        public LexicalCategory LexicalCategory { get; set; }
+
+        [JsonProperty("phrases")]
+        public List<Phras> Phrases { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+
+    public class Result
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("language")]
+        public string Language { get; set; }
+
+        [JsonProperty("lexicalEntries")]
+        public List<LexicalEntry> LexicalEntries { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("word")]
+        public string Word { get; set; }
+    }
+
+    public class OxfordDictionaryResponse
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("metadata")]
+        public Metadata Metadata { get; set; }
+
+        [JsonProperty("results")]
+        public List<Result> Results { get; set; }
+
+        [JsonProperty("word")]
+        public string Word { get; set; }
+    }
+}
---
+++ RandomWord.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace Company.Function
+{
+    public class RandomWord
+    {
+        [FunctionName("RandomWord")]
+        public async Task Run([TimerTrigger("0 * * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            HttpClient client = new HttpClient();
+
+            string randomWordAPI = "https://random-word-api.herokuapp.com/word?number=1";
+            var randomWordAPIResponse = await client.GetAsync(randomWordAPI);
+            var randomWordList = await randomWordAPIResponse.Content.ReadAsAsync<dynamic>();
+            string randomWord = randomWordList[0];
+            
+            string oxfordDictionaryAPI = "https://od-api.oxforddictionaries.com:443/api/v2/entries/en-gb/"+randomWord.ToLower();
+            string appId = Environment.GetEnvironmentVariable("OxfordAppId");
+            string apiKey = Environment.GetEnvironmentVariable("OxfordAppKey");
+            client.DefaultRequestHeaders.Add("app_id", appId);
+            client.DefaultRequestHeaders.Add("app_key", apiKey);
+            var oxfordDictionaryAPIResponse = await client.GetAsync(oxfordDictionaryAPI);
+            var wordMeaningJson = await oxfordDictionaryAPIResponse.Content.ReadAsStringAsync();
+            var wordMeaning = JsonConvert.DeserializeObject<OxfordDictionaryResponse>(wordMeaningJson);
+            
+            var twilioAccountSID = Environment.GetEnvironmentVariable("TwilioAccountSID");
+            var twilioAuthToken = Environment.GetEnvironmentVariable("TwilioAuthToken");
+            var twilioNumber = Environment.GetEnvironmentVariable("TwilioPhoneNumber");
+            var myNumber = Environment.GetEnvironmentVariable("MyPhoneNumber");
+            
+            string messageBody = $"WORD OF THE DAY\nThe word of the day is \"{randomWord}\", which means \"{wordMeaning.Results[0].LexicalEntries[0].Entries[0].Senses[0].Definitions[0]}\"";
+
+            TwilioClient.Init(twilioAccountSID, twilioAuthToken);
+
+            var message = MessageResource.Create(
+                body: messageBody,
+                from: new Twilio.Types.PhoneNumber(twilioNumber),
+                to: new Twilio.Types.PhoneNumber(myNumber)
+            );
+
+            log.LogInformation(message.Sid);
+        }
+    }
+}
